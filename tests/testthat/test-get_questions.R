865bc194a342ab3c9d4de926dcb69c4f15700177
Brian Krauth <bkrauth@sfu.ca>
2019-05-25 07:55:24 -0700

    Added default questions (SFU FASS) to get_questions()

--- tests/testthat/test-get_questions.R
+++ tests/testthat/test-get_questions.R
@@ -1,7 +1,5 @@
 test_that("Basic functionality", {
   expect_equal(nrow(get_questions("C:/Users/Brian/sfuvault/Chair/Teaching evaluations/SETC/Data/master/master question list.csv")), 19)
+  expect_equal(nrow(get_questions()), 15)
 })
 
-test_that("Fail cleanly", {
-  expect_error(get_questions())
-})
