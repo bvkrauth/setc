e718aae91c924ee5531e2b334abf7011f1d0d4e8
Brian Krauth <bkrauth@sfu.ca>
2019-05-26 14:09:34 -0700

    Added testing data and tests for get_instructors() and get_questions().

--- tests/testthat/test-get_instructors.R
+++ tests/testthat/test-get_instructors.R
@@ -1,3 +1,13 @@
 test_that("Basic functionality", {
-  expect_equal(nrow(get_instructors(instructor_file = "C:/Users/Brian/sfuvault/Chair/Teaching evaluations/SETC/Data/master/master instructor list.csv")), 48)
+  expect_equal(nrow(get_instructors("../data/master instructor list.csv")), 48)
+  expect_equal(nrow(get_instructors()), 0)
+  expect_equal(is_instructor_data(get_instructors("../data/master instructor list.csv")), TRUE)
+  expect_equal(is_instructor_data(get_instructors()), TRUE)
+})
+
+
+test_that("Fails cleanly", {
+  expect_error(get_instructors("../data/file that does not exist.csv")) # file that doesn't exist
+  #  expect_error(get_instructors("../data/1167 - Fall 2016/Brian Krauth (ECON 835 (G100) - Econometrics)_Fall_2016_ae41cc87-7609-4dbf-99bc-3c29f3c81e5ben-US.pdf")) # Not a CSV file
+  expect_error(get_instructors("../data/master question list.csv"))     # not an instructor list
 })
--- tests/testthat/test-get_questions.R
+++ tests/testthat/test-get_questions.R
@@ -1,4 +1,13 @@
 test_that("Basic functionality", {
-  expect_equal(nrow(get_questions("C:/Users/Brian/sfuvault/Chair/Teaching evaluations/SETC/Data/master/master question list.csv")), 19)
+  expect_equal(nrow(get_questions("../data/master question list.csv")), 19)
   expect_equal(nrow(get_questions()), 15)
+#  expect_equal(is_question_data(get_questions("../data/master question list.csv")), TRUE)
+#  expect_equal(is_question_data(get_questions()), TRUE)
 })
+
+test_that("Fails cleanly", {
+  expect_error(get_questions("../data/file that does not exist.csv")) # file that doesn't exist
+  #  expect_error(get_instructors("../data/1167 - Fall 2016/Brian Krauth (ECON 835 (G100) - Econometrics)_Fall_2016_ae41cc87-7609-4dbf-99bc-3c29f3c81e5ben-US.pdf")) # Not a CSV file
+#  expect_error(get_questions("../data/master instructor list.csv"))     # not a question list
+})
+
