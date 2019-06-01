253f812c125ab054837a2096e6c4d22a59484c89
Brian Krauth <bkrauth@sfu.ca>
2019-06-01 09:32:50 -0700

    Modified setc_save() so that it can accept lists.

--- R/setc_save.R
+++ R/setc_save.R
@@ -1,6 +1,20 @@
 #' Save SETC data in a convenient form.
 #'
-#' @param dat A tibble or data frame
+#' \code{setc_save} saves an SETC data object in both
+#' CSV and Rdata formats.
+#'
+#' If \code{dat} is a data frame (or tibble), \code{setc_save}
+#' will save \code{dat} in the files \code{[as]}.csv and
+#' \code{[as]}.Rdata, where \code{[as]} is the value of the
+#' \code{as = } argument.
+#'
+#' If \code{dat} is a list of data frames (or tibbles),
+#' \code{setc_save} will save \code{dat} in the file
+#' \code{[as]}.Rdata and each data frame in dat
+#' as the file \code{[as]}_\code{[data frame name]}.csv.
+#' .
+#'
+#' @param dat A tibble, data frame, or list of them.
 #'
 #' @param as A string indicating the type of data.
 #'
@@ -11,8 +25,17 @@
 #' @export
 #'
 setc_save <- function(dat, as, output_folder){
+  if (is.data.frame(dat)) {
+    dat %>%
+      readr::write_excel_csv(stringr::str_c(output_folder, as, ".csv"))
+  } else {
+    datnames <- names(dat)
+    for (i in 1:length(datnames)){
+      dat[[datnames[i]]] %>%
+        readr::write_excel_csv(stringr::str_c(output_folder, as, "_", datnames[i], ".csv"))
+    }
+  }
   dat %>%
-    readr::write_excel_csv(stringr::str_c(output_folder, as, ".csv")) %>%
     save(file = stringr::str_c(output_folder, as, ".RData"))
   invisible(dat)
 }
