60095fa34409b32d32171d6e51d9d36eb78a46ef
Brian Krauth <bkrauth@sfu.ca>
2019-06-06 16:04:50 -0700

    Added a proper test for setc_spread().

--- tests/testthat/test-setc_spread.R
+++ tests/testthat/test-setc_spread.R
@@ -1,3 +1,9 @@
-test_that("No tests yet", {
-  #  expect_equal(2 * 2, 4)
+test_that("Basic functionality", {
+  expect_equal(nrow(
+    setc_spread(dat = tibble::tibble(
+      course_id = c("ECON381D1Fa17","ECON381D1Fa17"),
+      question_id = c("QUAL","ATND"),
+      score_mean = c(3,NA)),
+      stat = "mean")),
+    1)
 })
