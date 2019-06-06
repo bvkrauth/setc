f07510488753dc501f93f14cbc9cb7f681784b27
Brian Krauth <bkrauth@sfu.ca>
2019-06-06 16:39:07 -0700

    Added a test for get_score_distribution().

--- tests/testthat/test-get_score_distribution.R
+++ tests/testthat/test-get_score_distribution.R
@@ -1,3 +1,17 @@
-test_that("No tests yet", {
-#  expect_equal(2 * 2, 4)
-})
+test_that("Basic functionality", {
+  expect_equal(get_score_distribution(
+    report_text = c("I attended class...",
+                    "       I attended class...",
+                    "           Distribution of Responses",
+                    "         Options                         Score Count Percentage",
+                    "         All of the time                     5      17         65.4%",
+                    "         Most of the time                    4        7        26.9%",
+                    "         About half of the time              3        1         3.8%",
+                    "         Rarely                              2        1         3.8%",
+                    "         Never                               1        0         0.0%"),
+    question = tibble::tibble(question_id = "ATND",
+                              question_text = "I attended class")),
+               c(0,1,1,7,17))
+  })
+
+
